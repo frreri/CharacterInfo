ea9238301eea71ccce3af64e69312e37147bd3a5
frreri <banox100@gmail.com>
2019-04-22 14:31:02 +0200

    Modifications to CSS

--- webengine/static/scripts/lookup.js
+++ webengine/static/scripts/lookup.js
@@ -1,5 +1,5 @@
 function search_char(){
-    document.getElementById("content-container").style.display = "none";
+    document.getElementsByClassName("hideable-content").style.display = "none";
     document.getElementById("load-anim").style.display = "block";
     var url="http://frer.se/lookup/" + document.getElementById("realm_c").value + "/" + document.getElementById("name_c").value.toLowerCase();
     location.href=url;
