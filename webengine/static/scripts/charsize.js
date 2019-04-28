1b968bb44973374f068c1772f838d3966cdde0ae
frreri <banox100@gmail.com>
2019-04-28 12:10:24 +0200

    modifications to charsize.js

--- webengine/static/scripts/charsize.js
+++ webengine/static/scripts/charsize.js
@@ -1,8 +1,15 @@
 function char_size(){
-    if (document.getElementById("c_race").textContent == 'Tauren' || document.getElementById("c_race").textContent == 'Orc') {
+
+    var size_L = ['Tauren', 'Orc'];
+    var size_M = ['Worgen', 'Draenei', 'Lightforged Draenei']
+
+    if (size_L.includes(document.getElementById("c_race").textContent)) {
         document.getElementById("picbox").style.backgroundSize ="620px 465px";
-    }
-    if ((document.getElementById("c_race").textContent == 'Worgen' || document.getElementById("c_race").textContent.includes('Draenei')) && document.getElementById("c_gender").textContent == 'Male'){
+    } else if (size_M.includes(document.getElementById("c_race").textContent) && document.getElementById("c_gender").textContent == 'Male') {
         document.getElementById("picbox").style.backgroundSize ="660px 495px";
     }
+
+    // if ((document.getElementById("c_race").textContent == 'Worgen' || document.getElementById("c_race").textContent.includes('Draenei')) && document.getElementById("c_gender").textContent == 'Male'){
+    //     document.getElementById("picbox").style.backgroundSize ="660px 495px";
+    // }
 }
