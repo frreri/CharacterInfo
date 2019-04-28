3e4e8c08556d82235fd5c4b0a4c341459599f34c
frreri <banox100@gmail.com>
2019-04-28 12:14:04 +0200

    modifications to charsize.js

--- webengine/static/scripts/charsize.js
+++ webengine/static/scripts/charsize.js
@@ -9,7 +9,4 @@ function char_size(){
         document.getElementById("picbox").style.backgroundSize ="660px 495px";
     }
 
-    // if ((document.getElementById("c_race").textContent == 'Worgen' || document.getElementById("c_race").textContent.includes('Draenei')) && document.getElementById("c_gender").textContent == 'Male'){
-    //     document.getElementById("picbox").style.backgroundSize ="660px 495px";
-    // }
 }
