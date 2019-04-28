b9a7c46a2830c128b48d5b23a8bb6207b3906676
frreri <banox100@gmail.com>
2019-04-28 12:12:37 +0200

    modifications to charsize.js

--- webengine/static/scripts/charsize.js
+++ webengine/static/scripts/charsize.js
@@ -1,7 +1,7 @@
 function char_size(){
 
     var size_L = ['Tauren', 'Orc'];
-    var size_M = ['Worgen', 'Draenei', 'Lightforged Draenei']
+    var size_M = ['Worgen', 'Draenei', 'Lightforged Draenei'];
 
     if (size_L.includes(document.getElementById("c_race").textContent)) {
         document.getElementById("picbox").style.backgroundSize ="620px 465px";
